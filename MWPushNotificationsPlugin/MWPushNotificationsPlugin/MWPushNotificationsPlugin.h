4d3c8cd8831e284746993edbcd2caaffe1231152
Xavi Moll <xmollv@gmail.com>
2020-12-09 16:02:55 +0100

    Cleaned the project and added the framework

---
+++ MWPushNotificationsPlugin/MWPushNotificationsPlugin/MWPushNotificationsPlugin.h
@@ -0,0 +1,18 @@
+//
+//  MWPushNotificationsPlugin.h
+//  MWPushNotificationsPlugin
+//
+//  Created by Xavi Moll on 9/12/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MWPushNotificationsPlugin.
+FOUNDATION_EXPORT double MWPushNotificationsPluginVersionNumber;
+
+//! Project version string for MWPushNotificationsPlugin.
+FOUNDATION_EXPORT const unsigned char MWPushNotificationsPluginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MWPushNotificationsPlugin/PublicHeader.h>
+
+
