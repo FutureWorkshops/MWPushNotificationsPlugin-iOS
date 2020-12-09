6124f219c4c5000b4f98bea9feab63ffa2ee8073
Xavi Moll <xmollv@gmail.com>
2020-12-09 16:10:46 +0100

    Added the skeleton for the push notifications view controller

---
+++ MWPushNotificationsPlugin/MWPushNotificationsPlugin/PushNotificationsStep/MWPushNotificationsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MWPushNotificationsViewController.swift
+//  MWPushNotificationsPlugin
+//
+//  Created by Xavi Moll on 9/12/20.
+//
+
+import UIKit
+import MobileWorkflowCore
+
+public class MWPushNotificationsViewController: ORKStepViewController {
+    
+}
